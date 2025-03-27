ec5e95cd70b048d27c5aa0e16f92ef52f53c5e7f
AugustinSeguin <augustinseguin@gmail.com>
2025-03-27 11:01:48 +0100

    jeu du simon en react done v1

---
+++ consignes.md
@@ -0,0 +1,49 @@
+Simon game
+
+Créer une application PWA
+
+Initialiser un projet avec vite :
+
+`npm create vite@latest`
+
+Ajouter le setup PWA :
+
+`npm install -D vite-plugin-pwa`
+
+Modifier le fichier vite.config.ts :
+
+
+```js
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My App',
+        background_color: "#ff0000"
+      }
+    })
+  ]
+})
+```
+
+
+Cahier des charges
+1 - Ajoutez l'implémentation du jeu du Simon Quatres couleurs sont disponibles : rouge, vert, bleu, jaune
+
+Le jeu doit afficher une séquence de couleurs à l'utilisateur 
+
+Puis l'utilisateur doit reproduire la séquence en cliquant sur les couleurs Si l'utilisateur se trompe, la partie est terminée Si l'utilisateur réussi, la séquence est augmentée d'une couleur 
+
+2 - Ajoutez des fonctionnalités "progressives" comme : 
+
+- la vibration à chaque nouvelle couleur lors de la séquence 
+- une notification locale à la fin de la partie avec le score 
+- la possibilité de saisir une couleur en la dictant plutôt qu'en cliquant sur la pastille 
+- stockage des meilleurs scores en local storage - un mode "dark" pour l'application en fonction de la luminosité ambiante - un son pour chaque couleur
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState, useEffect } from 'react'
+import './App.scss'
 
 function App() {
+  const [isStarted, setIsStarted] = useState(false)
   const [count, setCount] = useState(0)
+  const [playCount, setPlayCount] = useState(0)
+  const [historyColor, setHistoryColor] = useState<string[]>([])
+  const [currentColor, setCurrentColor] = useState<string>("")
+  const [message, setMessage] = useState<string>("")
+  const [isError, setIsError] = useState(false)
+  const [buttonMessage, setButtonMessage] = useState<string>("Commencer le jeu")
+
+  const clickButton = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement
+    const targetId = target.id
+    if (playCount < historyColor.length) {
+      if (targetId !== historyColor[playCount]) {
+        setMessage("Perdu !")
+        setIsError(true)
+        setPlayCount(0)
+        setCount(0)
+        setHistoryColor([])
+        setIsStarted(false)
+        setButtonMessage("Recommencer le jeu")
+      }
+    }
+    if (playCount + 1 == historyColor.length) {
+      setMessage("Bravo !")
+      setIsStarted(false)
+      setButtonMessage("Level up !")
+    }
+    setPlayCount(playCount + 1);
+  }, [historyColor, playCount, buttonMessage, isStarted, isError, message])
+
+  const addColor = useCallback(() => {
+    setPlayCount(0)
+    setCount(count + 1)
+    const colors = ["green", "red", "yellow", "blue"]
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]
+    setHistoryColor([...historyColor, randomColor])
+  }, [historyColor])
+
+  const startGame = useCallback(() => {
+    setIsStarted(true)
+    setMessage("")
+    addColor()
+    console.log(historyColor)
+    setCount(0)
+  }, [historyColor, addColor, count, isStarted])
+
+  useEffect(() => {
+    if (count < historyColor.length) {
+      setTimeout(() => {
+        setCurrentColor("")
+        setTimeout(() => {
+          setCurrentColor(historyColor[count])
+          console.log(`currentColor ${count} : ${historyColor[count]}`)
+          setCount(count + 1)
+        }, 50)
+      }, 500)
+    }
+  }, [historyColor, count])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Simon Game</h1>
+      <hr />
+      <h2>Niveau : {count}</h2>
+      <hr />
+
+      {message &&
+        <>
+          <p>{message}</p>
+          <hr />
+        </>
+      }
+
+      {!isStarted &&
+        <button
+          className="start"
+          onClick={() => {
+            startGame()
+          }}
+        >
+          {buttonMessage}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      }
+      <div className="card">
+        <div className="top">
+          <button
+            id="green"
+            className={currentColor === "green" ? "active" : ""}
+            onClick={clickButton}
+          ></button>
+          <button
+            id="red"
+            className={currentColor === "red" ? "active" : ""}
+            onClick={clickButton}
+          ></button>
+        </div>
+        <div className="bottom">
+          <button
+            id="yellow"
+            className={currentColor === "yellow" ? "active" : ""}
+            onClick={clickButton}
+          ></button>
+          <button
+            id="blue"
+            className={currentColor === "blue" ? "active" : ""}
+            onClick={clickButton}
+          ></button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
